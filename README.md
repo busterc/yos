f11ad2159354c9dbd88f15e9d742ce604ece0df6
Buster Collings <busterc@gmail.com>
2016-03-31 11:13:53 -0500

    heyo
    first

---
+++ README.md
@@ -0,0 +1,72 @@
+# yos
+> lists available yeoman generators and subgenerators
+
+## Install
+
+```sh
+$ npm install --global yos
+```
+
+## Usage
+
+```sh
+$ yos --help
+
+  Usage: yos [options] [generator-name ...]
+
+  Options:
+
+    -h, --help    show usage help
+
+```
+
+## Examples
+
+_show all available generators:_
+```sh
+$ yos
+Available Generators:
+
+  generator
+    subgenerator
+
+  nm
+
+  backbone
+    all
+    collection
+    model
+    router
+    view
+
+  node
+    boilerplate
+    cli
+    editorconfig
+    eslint
+    git
+    gulp
+    readme
+```
+
+_filtered:_
+```sh
+$ yos node nm
+Available Generators:
+
+  nm
+
+  node
+    boilerplate
+    cli
+    editorconfig
+    eslint
+    git
+    gulp
+    readme
+```
+
+
+## License
+
+ISC © [Buster Collings](http://about.me/buster)
---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const args = process.argv.slice(2);
+
+if (args.length && args[0] === '-h' || args[0] === '--help') {
+  console.log('\n  Usage: yos [options] [generator-name ...]\n');
+  console.log('  Options:\n\n    -h, --help    show usage help\n');
+  process.exit(1);
+}
+
+const env = require('yeoman-environment').createEnv();
+
+env.lookup(() => {
+  const generatorList = createGeneratorList(env, args);
+  console.log(`Available Generators:\n${generatorList}`);
+});
+
+function createGeneratorList(env, apps) {
+  const generators = Object.keys(env.getGeneratorsMeta()).reduce((namesByGenerator, generator) => {
+    const parts = generator.split(':');
+    const generatorName = parts.shift();
+
+    // Filter to specified generators, when specified
+    if (!apps.length || apps.indexOf(generatorName) > -1) {
+      // If first time we found this generator, prepare to save all its sub-generators
+      if (!namesByGenerator[generatorName]) {
+        namesByGenerator[generatorName] = [];
+      }
+      // If sub-generator (!== app), save it
+      if (parts[0] !== 'app') {
+        namesByGenerator[generatorName].push(parts.join(':'));
+      }
+    }
+    return namesByGenerator;
+  }, {});
+
+  if (!Object.keys(generators).length) {
+    return '\n  Couldn\'t find any generators, did you install any? Troubleshoot issues by running\n\n  $ yo doctor';
+  }
+
+  return Object.keys(generators).map(generator => {
+    const subGenerators = generators[generator].map(subGenerator => {
+      return `    ${subGenerator}`;
+    }).join('\n');
+
+    return `\n  ${generator}\n${subGenerators}`;
+  }).join('\n').replace(/\n\n\n/g, '\n\n');
+}
